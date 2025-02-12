f138513768c5a8701201ce472f39a5317e274e4f
Daniel Ching <dancwq2@gmail.com>
2025-02-12 20:48:47 +0800

    Added import statement

--- src/MapComponent.jsx
+++ src/MapComponent.jsx
@@ -4,6 +4,9 @@ import "leaflet/dist/leaflet.css";
 import "leaflet.markercluster/dist/MarkerCluster.css";
 import "leaflet.markercluster/dist/MarkerCluster.Default.css";
 import "leaflet.markercluster";
+import { useAnalytics } from './hooks/useAnalytics';
+
+
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
     iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
