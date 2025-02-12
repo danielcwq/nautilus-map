0cc7999036af02291963da792f1824b13ac2efb4
Daniel Ching <dancwq2@gmail.com>
2025-02-12 20:12:53 +0800

    changing icons

--- src/MapComponent.jsx
+++ src/MapComponent.jsx
@@ -4,6 +4,12 @@ import "leaflet/dist/leaflet.css";
 import "leaflet.markercluster/dist/MarkerCluster.css";
 import "leaflet.markercluster/dist/MarkerCluster.Default.css";
 import "leaflet.markercluster";
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+    iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+});
 
 const MapComponent = ({ data }) => {
     const mapRef = useRef(null);
